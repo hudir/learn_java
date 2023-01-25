3e3d45fb7cfb2571179ea3296d586e843e09a4ec
Zhuo <hudirybw@gmail.com>
2023-01-25 11:45:24 +0100

    update mortgageClac

--- mortgageCalc/mortgage/src/Main.java
+++ mortgageCalc/mortgage/src/Main.java
@@ -16,18 +16,29 @@ public class Main {
             System.out.print("Principal ($1K - %1M): ");
             principal = Double.parseDouble(scanner.nextLine().trim());
             if(principal >= 1000 && principal <= 1_000_000) break;
-            else System.out.println("Enter a number between 1,000 and 1,000,000.");
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
         }
 
         while (true) {
-            System.out.print("Annual Interest Rate (percent): ");
-            annualInterestRate = Double.parseDouble(scanner.nextLine().trim()) / percent / monthsInYear;
-            if(annualInterestRate >= 0 && annualInterestRate <= 30) break;
-            else System.out.println("Enter a number between 0 and 30.");
+            System.out.print("Annual Interest Rate (percent, 1%-30%): ");
+            annualInterestRate = Double.parseDouble(scanner.nextLine().trim());
+            if(annualInterestRate >= 1 && annualInterestRate <= 30) {
+                annualInterestRate = annualInterestRate / percent / monthsInYear;
+                break;
+            }
+            System.out.println("Enter a number between 0 and 30.");
         }
 
-        System.out.print("Period (Years): ");
-         period = Double.parseDouble(scanner.nextLine().trim()) * monthsInYear;
+        while (true) {
+            System.out.print("Period (Years, 1-30): ");
+            period = Double.parseDouble(scanner.nextLine().trim()) ;
+            if(period >= 1 && period <= 30) {
+                period = period * monthsInYear;
+                break;
+            }
+            System.out.println("Enter a number between 0 and 30.");
+        }
+
 
         final double pow = Math.pow(1 + annualInterestRate, period);
         final double result = principal * annualInterestRate * pow / (pow - 1);
